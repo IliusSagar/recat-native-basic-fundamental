cf7bc67b854c8117915ee5085e087fd2d4cf4a38
Md. Ilius Sagar <89338796+IliusSagar@users.noreply.github.com>
2024-12-02 12:28:52 +0600

    FlatList

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { View, Text, SafeAreaView, Image, ImageBackground, Pressable, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 import { StatusBar } from 'expo-status-bar'
-import { Button, Modal, Switch, TextInput } from 'react-native-web'
+import { Button, FlatList, Modal, Switch, TextInput } from 'react-native-web'
 
 // Text, View;
 // StatusBar;
@@ -18,39 +18,53 @@ import { Button, Modal, Switch, TextInput } from 'react-native-web'
 // RefreshController;
 // ActivityIndicator;
 
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
+
 const App = () => {
 
-const [text, setText] = useState("");
+
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
     <StatusBar barStyle="default" />
-    {/* <StatusBar barStyle="dark-content" /> */}
+
     
     <Text style={{ fontSize: 20, textAlign: 'center', marginTop: 50 }}>Hello World</Text>
-    
-    <Image style={{ width: 100, height: 50}} source={require("./assets/icon.png")}/>
-
-    {/* <Button title="Open Modal" onPress={() => setModal(true)} />
-    <Modal visible={modal} animationType="slide" transparent={false}>
-      <View>
-        <Text>This is a modal</Text>
-        <Button title="Close Modal" onPress={() => setModal(false)} />
-      </View>
-    </Modal> */}
-
-    <TextInput 
-    placeholder="Write here..." 
-    style={{ borderWidth: 2, borderColor: "red", width: "100%", padding: 10}}
-    value={text}
-    onChangeText={setText}
+   
+    <FlatList 
+    data={DATA} 
+    renderItem={({ item }) => <Item item={ item}/>} keyExtractor={(item) => item.id}
+    showsVerticalScrollIndicator={false}
+    contentContainerStyle={{
+      gap: 10,
+      marginTop: 30,
+    }}
     />
-
-    <Text>{text}</Text>
-
+    
 
   </SafeAreaView>
   )
 }
 
 export default App
+
+const Item = ({ item }) => {
+  return(
+    <View style={{ width: "100%", height: 300, padding: 20, backgroundColor: "blue"}}>
+      <Text style={{ color: "white"}}>{item.title}</Text>
+    </View>
+  )
+}
